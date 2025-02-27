80e86cb20822cb92a2dc860ba4662e176af69acb
ac1457 <chavanaadhira@gmail.com>
2025-02-27 17:06:02 +0000

    Update config.py

--- team_Ginkgo/modules/config.py
+++ team_Ginkgo/modules/config.py
@@ -1,3 +1,14 @@
+'''
+This file contains the configuration for the project.
+The main configuration settings include:
+- Google API Key
+- Google Custom Search Engine ID
+- Database Configuration
+- MINIO Configuration
+These settings are used by the different modules of the project to interact with external services and resources.
+For example, the Google API Key is used to access Google Custom Search API, the Database Configuration is used to connect to the SQL database, and the MINIO Configuration is used to store and retrieve files from the MINIO object storage service.
+
+'''
 # Google API Key
 GOOGLE_API_KEY = "AIzaSyCgYeXx0D-kHYOyCz1q0xsVwWq-g5lNLtg"
 
